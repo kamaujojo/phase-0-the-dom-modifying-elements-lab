73a6cb6b1628f796141d655f9fc1fa578e49da8b
joan <joan.kamau@student.moringaschool.com>
2024-07-21 01:23:33 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code here!
+document.querySelector("#main").remove();
+const newHeader = document.createElement("h1");
+newHeader.innerHTML = "Terence is the champion";
+newHeader.id = "victory"
